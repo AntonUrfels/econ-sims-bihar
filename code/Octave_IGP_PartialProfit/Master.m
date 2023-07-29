09170c1303c35dbd6bb2e0f2fffc0afc08feeedd
MaxwellMkondiwa <mkond001@umn.edu>
2023-07-29 21:54:50 +0530

    update

--- code/Octave_IGP_PartialProfit/Master.m
+++ code/Octave_IGP_PartialProfit/Master.m
@@ -9,11 +9,11 @@ ndataseq01060216=xlsread('rice_wheat_farmer_practice_fixedlong_rented_pprof_IGP.
 CreateWTPBoundsbyCell
 CreateTableData
 
-xlswrite('RA_rice_wheat_fixedlong_farmer_practice.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_farmer_practice_IGP.xlsx',RA)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_farmer_practice.xlsx',DescriptiveStat)
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_farmer_practice.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_farmer_practice_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_farmer_practice.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_farmer_practice.xlsx',PriceSensitivity)
@@ -26,14 +26,14 @@ CreateWTPBoundsbyCell
 CreateTableData
 
 %CreateFigureData
-xlswrite('RA_rice_wheat_fixedlong_fixedmedium.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_fixedmedium_IGP.xlsx',RA)
 #xlswrite('RAwOutliers.xlsx',RAwOutliers)
 %xlswrite('CELL30MIDMAP.xlsx',CELL30MIDMAP)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_fixedmedium.xlsx',DescriptiveStat)
 %xlswrite('Price_wheatSensitivity.xlsx',Price_wheatSensitivity)
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_fixedmedium.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_fixedmedium_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_fixedmedium.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_fixedmedium.xlsx',PriceSensitivity)
@@ -47,7 +47,7 @@ CreateWTPBoundsbyCell
 CreateTableData
 
 %CreateFigureData
-xlswrite('RA_rice_wheat_fixedlong_onset_long.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_onset_long_IGP.xlsx',RA)
 #xlswrite('RAwOutliers.xlsx',RAwOutliers)
 %xlswrite('CELL30MIDMAP.xlsx',CELL30MIDMAP)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_long.xlsx',DescriptiveStat)
@@ -55,7 +55,7 @@ xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_long.xlsx',DescriptiveStat)
 
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_onset_long.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_onset_long_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_onset_long.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_onset_long.xlsx',PriceSensitivity)
@@ -64,12 +64,12 @@ xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_onset_long.xlsx',PriceSens
 
 % 4 rice_wheat fixedlong versus onset medium
 clear
-ndataseq01060216=xlsread('rice_wheat_onset_medium_fixedlong_rented_pprof.xlsx')
+ndataseq01060216=xlsread('rice_wheat_onset_medium_fixedlong_rented_pprof_IGP.xlsx')
 CreateWTPBoundsbyCell
 CreateTableData
 
 %CreateFigureData
-xlswrite('RA_rice_wheat_fixedlong_onset_medium.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_onset_medium_IGP.xlsx',RA)
 #xlswrite('RAwOutliers.xlsx',RAwOutliers)
 %xlswrite('CELL30MIDMAP.xlsx',CELL30MIDMAP)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_medium.xlsx',DescriptiveStat)
@@ -77,44 +77,44 @@ xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_medium.xlsx',DescriptiveSta
 
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_onset_medium.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_onset_medium_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_onset_medium.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_onset_medium.xlsx',PriceSensitivity)
 
 % 5 rice_wheat fixedlong versus onset long suppl
 clear
-ndataseq01060216=xlsread('rice_wheat_onset_long_suppl_fixedlong_rented_pprof.xlsx')
+ndataseq01060216=xlsread('rice_wheat_onset_long_suppl_fixedlong_rented_pprof_IGP.xlsx')
 CreateWTPBoundsbyCell
 CreateTableData
 
 %CreateFigureData
-xlswrite('RA_rice_wheat_fixedlong_onset_long_suppl.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_onset_long_suppl_IGP.xlsx',RA)
 #xlswrite('RAwOutliers.xlsx',RAwOutliers)
 %xlswrite('CELL30MIDMAP.xlsx',CELL30MIDMAP)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_long_suppl.xlsx',DescriptiveStat)
 %xlswrite('Price_wheatSensitivity.xlsx',Price_wheatSensitivity)
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_onset_long_suppl.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_onset_long_suppl_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_onset_long_suppl.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_onset_long_suppl.xlsx',PriceSensitivity)
 
 % 6 rice_wheat fixedlong versus onset medium suppl
 clear
-ndataseq01060216=xlsread('rice_wheat_onset_medium_suppl_fixedlong_rented_pprof.xlsx')
+ndataseq01060216=xlsread('rice_wheat_onset_medium_suppl_fixedlong_rented_pprof_IGP.xlsx')
 CreateWTPBoundsbyCell
 CreateTableData
 
-xlswrite('RA_rice_wheat_fixedlong_onset_medium_suppl.xlsx',RA)
+xlswrite('RA_rice_wheat_fixedlong_onset_medium_suppl_IGP.xlsx',RA)
 #xlswrite('RAwOutliers.xlsx',RAwOutliers)
 %xlswrite('CELL30MIDMAP.xlsx',CELL30MIDMAP)
 xlswrite('DescriptiveStat_rice_wheat_fixedlong_onset_medium_suppl.xlsx',DescriptiveStat)
 %xlswrite('Price_wheatSensitivity.xlsx',Price_wheatSensitivity)
 
 clear
-RA=xlsread('RA_rice_wheat_fixedlong_onset_medium_suppl.xlsx')
+RA=xlsread('RA_rice_wheat_fixedlong_onset_medium_suppl_IGP.xlsx')
 CreateFigureData
 xlswrite('CELL30MIDMAP_rice_wheat_fixedlong_onset_medium_suppl.xlsx',CELL30MIDMAP)
 xlswrite('Price_wheatSensitivity_rice_wheat_fixedlong_onset_medium_suppl.xlsx',PriceSensitivity)
