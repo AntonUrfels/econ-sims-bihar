1e675b1c6d2178df934fab48834cf83b885faedc
MaxwellMkondiwa <mkond001@umn.edu>
2023-09-10 09:39:14 +0530

    update

--- code/Master.R
+++ code/Master.R
@@ -26,10 +26,24 @@ source("code/R_EIGP/2_WTP_results_fixedlongasbaseline_IGP.R")
 
 ## Yield 
 #Creates figures on yield WTP
-source("code/R_EIGP/Yield_postanalysis_IGP.R")
+source("code/R_EIGP/3_Yield_postanalysis_IGP.R")
+
+# Clean revenue data
+source("code/R_EIGP/4_Create_Revenue_Data_IGP.R")
+
+# clean revenue WTP IN Octave
+source("code/R_EIGP/5_Clean revenue for WTP in Octave_IGP.R")
 
 # 
-
+source("code/R_EIGP/6_WTP_results_fixedlongasbaseline_rev_IGP.R")
+#
+source("code/R_EIGP/7_Clean_partial_profit_for_WTP_in_Octave_IGP.R")
+#
+source("code/R_EIGP/8_WTP_results_fixedlongasbaseline_prof_IGP.R")
+#
+source("code/R_EIGP/9_Revenue_Partial_Profit_postanalysis_IGP.R")
+#
+source("code/R_EIGP/10_Optimal_pixellevel_strategy_IGP.R")
 
 
 
